e98c0b2d66f168a28c68c4a1a47d85f0f71ec449
Irevin <irevinnn@gmail.com>
2026-03-31 11:32:42 +0800

    lab09

---
+++ Labs/lab09/lab09.scm
@@ -0,0 +1,29 @@
+(define (over-or-under num1 num2) (cond ((< num1 num2) -1)
+                                        ((= num1 num2) 0)
+                                        ((> num1 num2) 1)))
+
+(define (make-adder num) (lambda (inc) (+ num inc)) )
+
+(define (composed f g) (lambda (x) (f (g x))))
+
+(define (repeat f n) (if (= n 1) 
+                        f
+                        (composed f (repeat f (- n 1)))))
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b)
+  (let ((big (max a b))
+        (small (min a b)))
+  (if (zero? (modulo big small))
+      small
+      (gcd small (modulo big small)))))
+
